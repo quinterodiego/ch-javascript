1a23d3aa01e90447bfa2ae4f932ba4027cfb7d3a
quinterodiego <d86webs@gmail.com>
2021-07-26 21:13:14 -0300

    quito las animaciones feas

--- Proyecto Final/js/manager.js
+++ Proyecto Final/js/manager.js
@@ -1,16 +1,18 @@
 CargarUsuarios();
 
 usuarioActual = JSON.parse(sessionStorage.getItem("UsuarioActual"));
-console.log("usuario: ", usuarioActual);
+
 UsuarioActual(usuarioActual);
 
 var idCuenta = 0;
+
+console.log();
 
 const AgregarCuenta = (event) => {
     idCuenta = usuarioActual.cuentas.length + 1;
     const id = idCuenta;
     const nombre = $('#nombre').val();
-    const importe = $('#importe').val();
+    const importe = new Number($('#importe').val()).toLocaleString("es-AR");
     const vencimiento = $('#vencimiento').val();
     const cuenta = new Cuenta(id, nombre, importe, vencimiento);
     usuarioActual.cuentas.push(cuenta);
